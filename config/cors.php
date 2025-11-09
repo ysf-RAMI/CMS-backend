6f12dbaaa0fde2e8ba739ec837fda7528174f72a
ysf-RAMI <youssef.rami.08@edu.uiz.ac.ma>
2025-11-09 01:37:07 +0100

    Update CORS to allow all Vercel deployments

--- config/cors.php
+++ config/cors.php
@@ -11,12 +11,15 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://cms-j621vn1fm-ramis-projects-4669c92d.vercel.app',
         'https://cms-4gva15tu5-ramis-projects-4669c92d.vercel.app',
-        'https://living-heddi-cmsbackend-6f6751c2.koyeb.app',
-        'https://cms-f.vercel.app/'
+        'https://cms-f.vercel.app',
+        'https://living-heddi-cmsbackend-6f6751c2.koyeb.app'
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.vercel\.app$/',
+    ],
 
     'allowed_headers' => ['*'],
 
