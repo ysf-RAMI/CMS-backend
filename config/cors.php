82fa59d44b5f3780abf337e05fb7a26bed1f67a4
ysf-RAMI <youssef.rami.08@edu.uiz.ac.ma>
2025-11-15 17:03:04 +0100

    Refactor CORS configuration to use environment variable for allowed origins

--- config/cors.php
+++ config/cors.php
@@ -8,15 +8,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'https://cms-j621vn1fm-ramis-projects-4669c92d.vercel.app',
-        'https://cms-4gva15tu5-ramis-projects-4669c92d.vercel.app',
-        'https://cms-f.vercel.app',
-        'https://living-heddi-cmsbackend-6f6751c2.koyeb.app',
-        'https://www.club-management-system.tech'
-    ],
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'))),
 
     'allowed_origins_patterns' => [
         '/^https:\/\/.*\.vercel\.app$/',
